f07c26a03c34f169e44fc5d6dac6323195698060
Michael Jang <michaeljang86@gmail.com>
2017-11-20 17:02:30 -0500

    Added backend tests for nearest burger

--- src/backend.test.js
+++ src/backend.test.js
@@ -19,6 +19,26 @@ describe('getCheapestBurger', () => {
   });
 })
 
+describe('getNearestBurger', () => {
+  it('returns a list of Burger objects', async () => {
+    var burgers = await foomenu.getNearestBurger()
+    expect(burgers.length).not.toBeUndefined()
+    expect(burgers[0]).toHaveProperty('name')
+    expect(burgers[0]).toHaveProperty('price')
+    expect(burgers[0]).toHaveProperty('restaurant')
+    expect(burgers[0].restaurant).toHaveProperty('name')
+    expect(burgers[0].restaurant).toHaveProperty('location')
+    expect(burgers[0].restaurant.location).toHaveProperty('lat')
+    expect(burgers[0].restaurant.location).toHaveProperty('lng')
+    expect(burgers[0]).toHaveProperty('distance')
+    expect(parseFloat(burgers[0].distance)).toBeInstanceOf('number')
+  });
+  it('sorts the list of Burger objects by distance, ascending', async () => {
+    var burgers = await foomenu.getNearestBurger()
+    expect(parseFloat(burgers[0].distance)).toBeLessThanOrEqual(parseFloat(burgers[burgers.length - 1].distance))
+  });
+})
+
 describe('getAllBurgers', () => {
   it('returns a list of Burger objects', async () => {
     var burgers = await foomenu.getAllBurgers()
