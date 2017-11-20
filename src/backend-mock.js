d990b652f10acacdb72eed4b4b704fd614cdd193
Michael Jang <michaeljang86@gmail.com>
2017-11-19 19:10:18 -0500

    Backend for Cheapest Burger mocked and tested

--- src/backend-mock.js
+++ src/backend-mock.js
@@ -2,11 +2,24 @@ class FoomenuAPI {
   getCheapestBurger = async () => {
     return await mockBurgerList
   }
+
+  getAllBurgers = async () => {
+    return await mockBurgerList
+  }
+
+  getRestaurants = async () => {
+    return await mockRestaurantList
+  }
 }
 
 
 export default new FoomenuAPI();
 
+var mockRestaurantList = [
+  { id: 'resto1', name: 'McDonalds' },
+  { id: 'resto2', name: 'A&W' },
+  { id: 'resto3', name: 'Burger King' },
+]
 
 var mockBurgerList = [
   {
@@ -14,6 +27,7 @@ var mockBurgerList = [
     price: 2.99,
     restaurant: {
       name: 'A&W',
+      location: { latitude: 45, longitude: 32 }
     }
   },
   {
@@ -21,13 +35,15 @@ var mockBurgerList = [
     price: 3.99,
     restaurant: {
       name: 'McDonalds',
+      location: { latitude: 45, longitude: 32 }
     }
   },
   {
     name: 'Momma Burger',
     price: 4.49,
     restaurant: {
-      name: 'A&W'
+      name: 'A&W',
+      location: { latitude: 45, longitude: 32 }
     }
   },
   {
@@ -35,6 +51,8 @@ var mockBurgerList = [
     price: 4.99,
     restaurant: {
       name: 'Burger King',
+      location: { latitude: 45, longitude: 32 }
     }
   }
 ]
+
