adaa26acb60d0f54ec05332f87a10975aea9a38e
Michael Jang <michaeljang86@gmail.com>
2017-11-22 20:38:29 -0500

    Reformatted indentation and updated package.json

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
-    _handleClick = () => {
-        this.props.updateUser({ userID: null });
+  _handleClick = () => {
+    this.props.updateUser({ userID: null });
+  }
+
+  render() {
+    if (!this.props.userID) {
+      return (
+        <div className="navBar">
+          <div className="navBarRight">
+            <Link className="navBarLinks" to='/login'>Login</Link>
+          </div>
+        </div>
+      );
     }
+    return (
+      <div className="navBar">
+        <div className="navBarLeft">
+          <Link className="navBarLinks" to='/myAccount'>My Account</Link>
+        </div>
+        <div className="navBarRight">
+          <Link className="navBarLinks" to='/' onClick={this._handleClick}>Sign Out</Link>
+        </div>
+      </div>
 
-    render() {
-        if (!this.props.userID) {
-            return (
-                <div className="navBar">
-                    <div className="navBarRight">
-                        <Link className="navBarLinks" to='/login'>Login</Link>
-                    </div>
-                </div>
-            );
-        }
-        return (
-            <div className="navBar">
-                <div className="navBarLeft">
-                    <Link className="navBarLinks" to='/myAccount'>My Account</Link>
-                </div>
-                <div className="navBarRight">
-                    <Link className="navBarLinks" to='/' onClick={this._handleClick}>Sign Out</Link>
-                </div>
-            </div>
+    )
 
-        )
-
-    }
+  }
 
 }
 
